74349c71e806c3ffb460b0deaa459717757dff8a
LauIbarra03 <ibarralaureano332@gmail.com>
2023-08-27 00:15:31 -0300

    fix al login

--- Login/scripts/password.js
+++ Login/scripts/password.js
@@ -9,33 +9,21 @@ const parrafo = document.getElementById("warnings")
 
 form.addEventListener("submit", e=> {
   e.preventDefault()
-  let entrar = false
-  let warnings = ""
   let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-  parrafo.innerHTML = ""
 
   
   if(!regexEmail.test(email.value)){
-    warnings += `Email no valido (primer item) <br>`
-    entrar = true
+    alert("Email no valido (primer item)")
+  }
+  else if(dni.value.length < 4){
+    alert("DNI no valido (segundo item)")
+  }
+  else if(password1.value != password2.value || password1.value.length < 8 || password2.value.length < 8){
+    alert("Las contraseñas no coinciden o no son validas, debe tener mas de 8 caracteres")
+  }
+  else {
+   alert("Enviado con éxito")
   }
 
-  if(dni.value.length < 4){
-    warnings += `DNI no valido (segundo item)  <br>`
-    entrar = true
-  }
-
-  if(password1.value != password2.value || password1.value.length < 8 || password2.value.length < 8){
-    warnings += `Las contraseñas no coinciden o no son validas, debe tener mas de 8 caracteres<br>`
-    entrar = true
-  }
-
-
-  if(entrar){
-    parrafo.innerHTML = warnings
-  } 
-  else{
-    parrafo.innerHTML = "Enviado con éxito"
-  }
 })
 
