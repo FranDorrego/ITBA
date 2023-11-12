f05c6ec7e5ff6a3d89bd427ff18522260bb7dc45
FranDorrego <francodorrego@gmail.com>
2023-11-12 18:02:30 -0300

    home vista

--- sprint_7/itbank/home/views.py
+++ sprint_7/itbank/home/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    context = {
+        "user": "carlos",
+        "actualCuenta": 123456,
+        "cantidadIngreso": 123,
+        "cantidadEgreso": 456,
+        "movimientos" : [
+            {
+                "fecha": '12/12/12',
+                "tipo": 'Salida',
+                "motivo": 'Pago de tarjeta',
+                "cantidad": 123456
+            }
+        ],
+        }
+    return render(request, "home/home.html", context)
--- sprint_7/itbank/itbank/urls.py
+++ sprint_7/itbank/itbank/urls.py
@@ -17,11 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import login.views as login
+import home.views as home
 
 urlpatterns = [
     path('', login.login, name=""),
     path('password', login.password, name="password"),
     path('register', login.register, name="register"),
+
+    path('home', home.home, name="home"),
 
     path('admin/', admin.site.urls),
 ]
