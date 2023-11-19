3a9442302d4ee9a837c5913ea6287fbbef1bd4f8
LauIbarra03 <ibarralaureano332@gmail.com>
2023-11-19 17:31:41 -0300

    relacion de tabla de usuarios con tabla de clientes hecha

--- sprint_7/itbank/login/views.py
+++ sprint_7/itbank/login/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required 
 from django.contrib.auth import login as auth_login, logout as logout_user, authenticate
+from django.apps import apps
 
 # Create your views here.
 def login(request):
+
+    # Obtén todas las aplicaciones instaladas en tu proyecto
+    installed_apps = apps.get_app_configs()
+
+    # Itera sobre cada aplicación
+    for app_config in installed_apps:
+        # Obtén todos los modelos definidos en la aplicación
+        models = app_config.get_models()
+
+        # Itera sobre cada modelo en la aplicación
+        for model in models:
+            # Haz lo que necesites con el modelo
+            print(f"Modelo: {model.__name__}, App: {app_config.name}")
+    
     if request.method == 'GET':
         return render(request, 'login/login.html')
     else:
